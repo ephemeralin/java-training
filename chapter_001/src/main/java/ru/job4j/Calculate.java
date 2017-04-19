be43fee09e62d5c95ddc7432874dbcd2152d6ff2
ephemeralin <ephemeralin@gmail.com>
2017-04-19 13:20:39 +0300

    task 4087 _

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j;
 
+/**
+* Class Calculate - класс для вывода строки "Hello world!" на экран
+* @author vpiliugin
+* @since 19.04.2017
+*/
 public class Calculate {
+  //**
+  * Конструктор, вывод строки в консоль
+  * @param args - аргументы, переданные в программу в виде ключей запуска
+  *//
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
