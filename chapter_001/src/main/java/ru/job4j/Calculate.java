f9b0bd5057f7d10b98854ee0c3d3dd5b7cedf24f
ephemeralin <ephemeralin@gmail.com>
2017-04-19 16:17:54 +0300

    task 4087 /bug with double slash was fixed

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -6,10 +6,10 @@ package ru.job4j;
 * @since 19.04.2017
 */
 public class Calculate {
-  //**
+  /**
   * Конструктор, вывод строки в консоль
   * @param args - аргументы, переданные в программу в виде ключей запуска
-  *//
+  */
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
