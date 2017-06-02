ffa45b7f77d0370588749ddbfbcaf1de4ce2d4a2
ephemeralin <ephemeralin@gmail.com>
2017-06-02 22:12:27 +0300

    task 785 /Stub implementation /added more tests

--- chapter_002/src/test/java/ru.job4j/tracker/StubInputTest.java
+++ chapter_002/src/test/java/ru.job4j/tracker/StubInputTest.java
@@ -12,7 +12,7 @@ public class StubInputTest {
 
     /**
      * When add new item then tracker has same new item with the same name.
-     * Test for methods: add, findAll
+     * Test for methods: add, findAll, exit
      */
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
@@ -21,6 +21,36 @@ public class StubInputTest {
         Input input = new StubInput(answers);
         new StartUI(tracker, input).init();
         assertThat(tracker.findAll()[0].getDesc(), is("Test description"));
+
+    }
+
+    /**
+     * When add new item and edit its desc then tracker has item with another desc.
+     * Test for methods: add, findAll, edit, exit
+     */
+    @Test
+    public void whenUserAddItemAndEditItsDescThenTrackerHasItemWithAnotherDesc() {
+        Tracker tracker = new Tracker();
+        String[] answers = {"0", "John Doe", "Test description", "4", "task0",
+                "2", "War and Peace", "6"};
+        Input input = new StubInput(answers);
+        new StartUI(tracker, input).init();
+        assertThat(tracker.findAll()[0].getDesc(), is("War and Peace"));
+    }
+
+    /**
+     * When add two new items and delete first then tracker has only
+     * second item.
+     * Test for methods: add, findAll, findByID, delete, exit
+     */
+    @Test
+    public void whenUserAddTwoItemsAndDeleteFirstThenTrackerHasOnlySecondOne() {
+        Tracker tracker = new Tracker();
+        String[] answers = {"0", "John Doe", "Test description 1", "0",
+                "Bob Marley", "Test description 2", "4", "task0", "3", "6"};
+        Input input = new StubInput(answers);
+        new StartUI(tracker, input).init();
+        assertThat(tracker.findAll()[0].getDesc(), is("Test description 2"));
 
     }
 
