facea5e7df9b514bdb191516adb47d54fb81b4c1
ephemeralin <ephemeralin@gmail.com>
2017-07-01 21:47:28 +0300

    task 792 /Final task (Chess moves) /start developing

---
+++ chapter_002/src/main/java/ru.job4j/chessMoves/Board.java
@@ -0,0 +1,14 @@
+package ru.job4j.chessMoves;
+
+public class Board {
+    private static int SIZE_OF_BOARD = 8;
+
+    private Cell[][] cells;
+
+    public static class Cell {
+    }
+
+    public Board() {
+
+    }
+}
