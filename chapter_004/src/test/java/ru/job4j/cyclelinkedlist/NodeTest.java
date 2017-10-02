6185d2c824f85940bda563a50ecb109e7c1a884c
ephemeralin <ephemeralin@gmail.com>
2017-10-02 17:43:35 +0300

    task 161 /Cycle determination of the custom linked list /done

---
+++ chapter_004/src/test/java/ru/job4j/cyclelinkedlist/NodeTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.cyclelinkedlist;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * The type Node test.
+ */
+public class NodeTest {
+    /**
+     * Has cycle.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void hasCycle() throws Exception {
+        Node first = new Node(1);
+        Node two = new Node(2);
+        Node third = new Node(3);
+        Node four = new Node(4);
+
+        first.setNext(two);
+        two.setNext(third);
+        third.setNext(four);
+        four.setNext(first);
+
+        assertThat(Node.hasCycle(first), is(true));
+    }
+
+}
