a390fb258f7a17b79d2a090fcd550672a26ef399
ephemeralin <ephemeralin@gmail.com>
2017-10-04 18:22:16 +0300

    task 157 /Generics /fixed

--- chapter_004/src/test/java/ru/job4j/generics/UserStoreTest.java
+++ chapter_004/src/test/java/ru/job4j/generics/UserStoreTest.java
@@ -11,16 +11,16 @@ import static org.junit.Assert.assertThat;
  */
 public class UserStoreTest {
     /**
-     * When add role then find by id it.
+     * When add user then find by id it.
      *
      * @throws Exception the exception
      */
     @Test
-    public void whenAddRoleThenFindByIdIt() throws Exception {
-        UserStore userStore = new UserStore(3);
-        userStore.add(new Role("a1", "manager"));
-        Role role = (Role) userStore.findById("a1");
-        assertThat(role.getId(), is("a1"));
+    public void whenAddThenFindByIdIt() throws Exception {
+        GeneralStore<User> userStore = new GeneralStore<User>(3);
+        userStore.add(new User("a1", "manager"));
+        User user = (User) userStore.findById("a1");
+        assertThat(user.getId(), is("a1"));
     }
 
     /**
@@ -30,11 +30,11 @@ public class UserStoreTest {
      */
     @Test
     public void whenUpdateThenFindByIdItWithOtherName() throws Exception {
-        UserStore userStore = new UserStore(3);
-        userStore.add(new Role("a1", "manager"));
-        Role role = (Role) userStore.findById("a1");
-        role.setName("director");
-        userStore.update(role);
+        GeneralStore<User> userStore = new GeneralStore<User>(3);
+        userStore.add(new User("a1", "manager"));
+        User user = (User) userStore.findById("a1");
+        user.setName("director");
+        userStore.update(user);
         assertThat(userStore.findById("a1").getName(), is("director"));
     }
 
@@ -45,8 +45,8 @@ public class UserStoreTest {
      */
     @Test
     public void whenAddAndDeleteOneThenHasNull() throws Exception {
-        UserStore userStore = new UserStore(3);
-        userStore.add(new Role("a1", "manager"));
+        GeneralStore<User> userStore = new GeneralStore<User>(3);
+        userStore.add(new User("a1", "manager"));
         userStore.delete("a1");
         assertNull(userStore.findById("a1"));
     }
